7fff70a2ec2d9ab5343580c03fcbe0e40636c895
alexpeachey <alex.peachey@gmail.com>
2020-08-19 16:32:49 -0700

    Step 5 Expand the GenericModule to use the new Repo

--- lib/nook_book/data/generic_cache.ex
+++ lib/nook_book/data/generic_cache.ex
@@ -1,6 +1,7 @@
 defmodule NookBook.Data.GenericCache do
   @behaviour NookBook.Data.TableBehaviour
   require Record
+  alias NookBook.Data.Repo
 
   Record.defrecord(:generic_cache, key: nil, value: nil)
 
@@ -8,4 +9,52 @@ defmodule NookBook.Data.GenericCache do
   def table_type(), do: :set
   def table_fields(), do: [:key, :value]
   def table_indexes(), do: []
+
+  def get(key) do
+    case Repo.read(table_name(), key) do
+      {:atomic, [data]} -> generic_cache(data, :value)
+      {:atomic, []} -> nil
+    end
+  end
+
+  def set(_key, nil), do: nil
+
+  def set(key, value) do
+    Repo.write(generic_cache(key: key, value: value))
+    value
+  end
+
+  def remove(key) do
+    Repo.delete(table_name(), key)
+  end
+
+  def clear() do
+    Repo.clear(table_name())
+  end
+
+  def all() do
+    table_name()
+    |> Repo.all()
+    |> case do
+      {:atomic, list} ->
+        list
+        |> Enum.map(&generic_cache(&1, :value))
+
+      _ ->
+        []
+    end
+  end
+
+  def filter(pattern) do
+    table_name()
+    |> Repo.filter({table_name(), pattern, :_})
+    |> case do
+      {:atomic, list} ->
+        list
+        |> Enum.map(&generic_cache(&1, :value))
+
+      _ ->
+        []
+    end
+  end
 end
